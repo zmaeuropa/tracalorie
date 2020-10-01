d1f5278f0bfb24455142cf0198f13ac7789ddffc
zmaeuropa <smain.rabhi@gmail.com>
2020-10-01 19:47:10 +0200

    sets some items

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ const ItemCtrl = (function(){
         this.name = name;
         this.calories = calories;
     }
+
+    // Data Structure / state
+    const state = {
+        items: [
+            {id:0, name: 'Steak Dinner', calories: 800},
+            {id:1, name: 'Hamburger', calories: 950},
+            {id:2, name: 'Salade', calories: 300}
+        ],
+        currentItem = null,
+        totalCalories = 0
+    }
+
  
 })();
 
