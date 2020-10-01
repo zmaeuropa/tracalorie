acd5c08d00e3d2e9fd5dde64c387a229429cb664
zmaeuropa <smain.rabhi@gmail.com>
2020-10-01 19:38:47 +0200

    add: script link & controller js

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 // Storage Controller
 
 // Item Controller 
+const ItemCtrl = (function(){
+ 
+})();
 
 // UI Controller
+const UiCtrl = (function(){
+ 
+})();
 
 // App Controller
+const App = (function(ItemCtrl, UiCtrl){
+ 
+})(ItemCtrl, UiCtrl);
